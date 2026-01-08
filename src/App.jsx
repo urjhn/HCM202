7a948be4d0272ddf9012041d50dccbf91f1eef4c
LeTrucAn <letrucan096666@gmail.com>
2026-01-09 06:51:20 +0700

    fix: fix 3D

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from 'react'
 import Navbar from './components/Navbar'
 import ContentSection from './components/ContentSection'
 import TimelineSection from './components/TimelineSection'
 import QuizButton from './components/QuizButton'
 import QuizGame from './components/QuizGame'
+import ThreeBackground from './components/ThreeBackground'
+import ThreeBackgroundButton from './components/ThreeBackgroundButton'
 
 function App() {
+  const [isThreeBackgroundOpen, setIsThreeBackgroundOpen] = useState(false)
+
   const handleOpenQuiz = async () => {
     try {
       const quizGame = new QuizGame()
@@ -20,6 +25,12 @@ function App() {
       <Navbar />
       <ContentSection />
       <TimelineSection />
+
+      {isThreeBackgroundOpen && (
+        <ThreeBackground onClose={() => setIsThreeBackgroundOpen(false)} />
+      )}
+
+      <ThreeBackgroundButton onClick={() => setIsThreeBackgroundOpen(true)} />
       <QuizButton onClick={handleOpenQuiz} />
     </div>
   )
